1f456785a3197cebc62af8d53a2e7c2a102ecec4
taetaerin <taerin13@naver.com>
2024-01-03 13:38:22 +0900

    feat: 카테고리 별 도서 조회, 카테고리 목록 조회

--- controller/BookController.js
+++ controller/BookController.js
@@ -2,17 +2,38 @@ const conn = require('../mariadb');
 const {StatusCodes} = require('http-status-codes');
 
 const allBooks = (req, res) => {
-    let sql = `SELECT * FROM books`;
-    conn.query(sql, 
-        (err, results) => {
-            if(err) {
-                console.log(err)
-                return res.status(StatusCodes.BAD_REQUEST).end();
+    let {category_id} = req.query;
+
+    if(category_id) {
+        let sql = `SELECT * FROM books WHERE category_id=?`;
+        conn.query(sql, category_id,
+            (err, results) => {
+                if(err) {
+                    console.log(err)
+                    return res.status(StatusCodes.BAD_REQUEST).end();
+                }
+            
+                if(results.length) {
+                    return res.status(StatusCodes.OK).json(results);
+                }else {
+                    return res.status(StatusCodes.NOT_FOUND).end();
+                }
             }
-            return res.status(StatusCodes.OK).json(results);
-        }
-    )
-}
+        )
+    }
+    else {
+        let sql = `SELECT * FROM books`;
+        conn.query(sql, 
+            (err, results) => {
+                if(err) {
+                    console.log(err)
+                    return res.status(StatusCodes.BAD_REQUEST).end();
+                }
+                return res.status(StatusCodes.OK).json(results);
+            }
+        )
+    }
+};
 
 const bookDetail = (req, res) => {
     let {id} = req.params;
@@ -34,8 +55,5 @@ const bookDetail = (req, res) => {
     )
 };
 
-const booksByCategory = (req, res) => {
-    res.json('카테고리별 신간 도서 조회');
-}
 
-module.exports = {allBooks, bookDetail, booksByCategory};
+module.exports = {allBooks, bookDetail};
---
+++ routes/category.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const {allCategory} = require('../controller/CategoryController');
+
+router.use(express.json())
+
+router.get('/', allCategory );
+
+module.exports = router;
