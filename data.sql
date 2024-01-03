dfddce1d0ba61f82f476afe464e93a1aa8bb889c
taetaerin <taerin13@naver.com>
2024-01-03 10:58:54 +0900

    add: data.sql

---
+++ data.sql
@@ -0,0 +1,16 @@
+INSERT INTO books (title, form, isbn, summary, detail, author, pages, contents, price, pub_date)
+VALUES ('어린왕자', '종이책', '0', '어린 왕자는 작은 왕국에서 왔다.', '어린 왕자는 작은 왕국에서 왔다. 그는 여행 중 다양한 이야기를 만나면서 성장한다.',
+'김왕자', 100, '목차', 20000, '2019-01-01');
+
+INSERT INTO books (title, form, isbn, summary, detail, author, pages, contents, price, pub_date)
+VALUES ('신데렐라', '종이책', '1','신데렐라는 부모님이 죽은 뒤 어려운 삶을 살지만 마법을 통해 공주로 변하게 된다.', '신데렐라는 부모님이 죽은 뒤 어려운 삶을 살지만 마법을 통해 공주로 변하게 된다. 전통적인 동화의 대표적인 이야기 중 하나이다.',
+'김델라', 150, '목차', 19000, '2023-12-01');
+
+INSERT INTO books (title, form, isbn, summary, detail, author, pages, contents, price, pub_date)
+VALUES ('백설공주', '종이책', '2', '매서운 악행에 시달리던 백설공주는 세 난쟁이와 함께 평화로운 삶을 찾아가는 이야기.', '매서운 악행에 시달리던 백설공주는 세 난쟁이와 함께 평화로운 삶을 찾아가는 이야기. 그녀의 아름다움이 동화의 주요 테마 중 하나이다.',
+'김백설', 120, '목차', 22000, '2023-11-01');
+
+INSERT INTO books (title, form, isbn, summary, detail, author, pages, contents, price, pub_date)
+VALUES ('흥부와 놀부', '종이책', '3', '흥부와 놀부는 형제지만 운명은 매우 다르게 펼쳐진다. 성실함과 게으름의 결말을 다룬 전래동화.',
+'흥부와 놀부는 형제지만 운명은 매우 다르게 펼쳐진다. 성실함과 게으름의 결말을 다룬 전래동화.',
+'김덕수', 205, '목차', 20000, '2023-12-08');
