fc1772d09c33bfb0deb26d98d3886ee061092e23
taetaerin <taerin13@naver.com>
2024-01-05 13:01:25 +0900

    feat: 좋아요 추가/삭제, 좋아요 개수, 좋아요 여부

--- routes/likes.js
+++ routes/likes.js
@@ -1,18 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const {addLike, removeLike} = require('../controller/LikeController');
 
 router.use(express.json())
 
-router.post('/:id', (req, res) => {
-    res.json('좋아요 추가');
-});
-
-
-router.delete('/:id', (req, res) => {
-    res.json('좋아요 취소');
-});
-
-
-
+router.post('/:id', addLike);
+router.delete('/:id', removeLike);
 
 module.exports = router;
