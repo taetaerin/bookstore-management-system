f7eaa776c61b6c5a4ab185b9fee450357269af66
taetaerin <taerin13@naver.com>
2024-01-13 20:37:52 +0900

    refactor: 카테고리 관련 sql문 모듈화

--- controller/CategoryController.js
+++ controller/CategoryController.js
@@ -1,16 +1,14 @@
 import { StatusCodes } from "http-status-codes";
-import conn from "../mariadb.js";
+import { getCategories } from "../services/CategoryService.js";
 
-const allCategory = (req, res) => {
-    let sql = `SELECT * FROM category`;
-    conn.query(sql, (err, results) => {
-        if (err) {
-            console.log(err);
-            return res.status(StatusCodes.BAD_REQUEST).end();
-        }
-
+const allCategory = async (req, res) => {
+    try {
+        const results = await getCategories();
         return res.status(StatusCodes.OK).json(results);
-    });
+    } catch (err) {
+        console.log(err);
+        return res.status(StatusCodes.BAD_REQUEST).end();
+    }
 };
 
 export { allCategory };
---
+++ services/CategoryService.js
@@ -0,0 +1,22 @@
+import mariadb from "mysql2/promise";
+
+const getCategories = async () => {
+    const conn = await mariadb.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "root",
+        database: "Library",
+        dateStrings: true,
+    });
+
+    try {
+        const sql = "SELECT * FROM category";
+        const [rows, fields] = await conn.execute(sql);
+        return rows;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+};
+
+export { getCategories };
